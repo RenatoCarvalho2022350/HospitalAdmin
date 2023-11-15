aa6e87fe0bdf6c5a108dcdef4cc45cd3baec89cf
RenatoCarvalho2022350 <146198748+RenatoCarvalho2022350@users.noreply.github.com>
2023-11-15 12:28:11 +0000

    Initial commit

---
+++ src/hospitaladmin/Patient.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitaladmin;
+
+/**
+ *
+ * @author Renato
+ */
+public class Patient {
+    /*
+    Name
+    Date bith
+    blood type
+    ID
+    */
+    
+    private String name;
+    private String birthD;
+    private String bloodType;
+    private int patientID;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthD() {
+        return birthD;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public int getPatientID() {
+        return patientID;
+    }
+    
+    
+    
+    
+    
+}
